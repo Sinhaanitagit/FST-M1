6832a15344e981714613d28bb4dc39cbb4ff64f6
Sinhaanitagit <84303316+Sinhaanitagit@users.noreply.github.com>
2021-05-17 11:37:52 +0530

    Add files via upload

---
+++ sql/Activity1.sql
@@ -0,0 +1,20 @@
+REM   Script: activity1
+REM   Activity1
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+	 
+    salesman_id int, 
+	 
+    salesman_name varchar2(32), 
+	 
+    salesman_city varchar2(32), 
+	 
+    commission int 
+	 
+);
+
+desc salesman
+
+
